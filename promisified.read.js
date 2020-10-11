c639409dd0b9c8e68b1a0f70c8a95cb4b4baa33b
ddotx <domemacintosh@gmail.com>
2020-10-11 13:37:30 +0700

    add more code

--- promisified.read.js
+++ promisified.read.js
@@ -4,6 +4,10 @@ const {promisify} = require('util');
 
 const readFile = promisify(fs.readFile);
 
+/* readFile("./data/pulitzer-circulation-data.csv", "utf8")
+.then(data => console.table(convertCsv(data)))
+.catch(err => console.log(`File error: ${err}`)) */
+
 const read = async () => {
     const data = await readFile("./data/pulitzer-circulation-data.csv", "utf8");
     console.table(convertCsv(data));
